ecd4ceeb6a9685abb842ae76f99228f5616e82e5
Hyeonkwon Cho <chkwon91@gmail.com>
2018-12-20 22:24:00 +0900

    ubuntu: Use environment variable to express disk directory

--- ubuntu-18.04/launch_ubuntu.sh
+++ ubuntu-18.04/launch_ubuntu.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
+DISK_PATH=/home/kwony/TASKSPACE/VM/ubuntu-18.04
 
 qemu-system-x86_64 \
         -cpu host \
         -smp 8 \
         -vga std -enable-kvm -m 2048 \
         -monitor stdio \
-        -drive file=ubuntu.qcow2,cache=none \
-        -display gtk \
+        -drive file=${DISK_PATH}/ubuntu.qcow2,cache=none \
         -D log.d \
